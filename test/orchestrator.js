dc9ae9528818fca79cd6f7af6ecbb655f0acc099
Matheus Henrique <matt.hcsantos@gmail.com>
2026-04-03 01:31:59 -0300

    feat: implement email service with nodemailer and add integration tests

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -2,8 +2,11 @@ import retry from "async-retry";
 import database from "infra/database.js";
 import generateMigration from "models/migrator.js";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fechStatusPage, {
@@ -21,6 +24,41 @@ async function waitForAllServices() {
   }
 }
 
+async function waitForEmailServer() {
+  return retry(fechStatusPage, {
+    retries: 100,
+    maxTimeout: 1000,
+  });
+
+  async function fechStatusPage() {
+    const response = await fetch(`${emailHttpUrl}`);
+
+    if (response.status !== 200) {
+      throw Error();
+    }
+  }
+}
+
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, {
+    method: "DELETE",
+  });
+}
+
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+  const emailListBody = await emailListResponse.json();
+  const lastEmailItem = emailListBody.pop();
+
+  const emailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+  const emailTextBody = await emailTextResponse.text();
+
+  lastEmailItem.text = emailTextBody;
+  return lastEmailItem;
+}
+
 async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
@@ -33,4 +71,6 @@ export default {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
+  deleteAllEmails,
+  getLastEmail,
 };
